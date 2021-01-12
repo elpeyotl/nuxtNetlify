cdb44c122bfdfc0b3ca893859e317d76505f029f
elpeyotl <ch.perez@mac.com>
2021-01-13 00:35:28 +0100

    hossa

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -3,6 +3,9 @@
     <h2>{{ post.title }}</h2>
     <nuxt-content :document="post" />
     <img :src="post.thumbnail" />
+    <div v-for="album in albums" :key="album.slug">
+      {{ album.title }} {{ album.artist }}
+    </div>
   </div>
 </template>
 
@@ -10,15 +13,23 @@
 export default {
   async asyncData({ $content, params, error }) {
     let post
+    let albums
     try {
       post = await $content('blog', params.slug).fetch()
       // OR const article = await $content(`articles/${params.slug}`).fetch()
     } catch (e) {
       error({ message: 'Blog Post not found' })
     }
-
+    try {
+      albums = await $content('albums')
+        .where({ slug: { $in: post.albums } })
+        .fetch()
+    } catch (e) {
+      error({ message: 'Blog Post not found' })
+    }
     return {
       post,
+      albums,
     }
   },
 }
