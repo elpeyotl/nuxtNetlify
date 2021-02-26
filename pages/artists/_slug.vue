90e96ebf68c0429b72ad427b8a9ca8eea21d47b7
elpeyotl <ch.perez@mac.com>
2021-02-26 23:16:13 +0100

    artis page responsive

--- pages/artists/_slug.vue
+++ pages/artists/_slug.vue
@@ -9,13 +9,15 @@
       <img :src="artist.headerImage" />
     </div>
     <h2 class="text-2xl w-1/2 font-semibold mb-8">{{ artist.artist }}</h2>
-    <div class="mb-16 flex">
-      <div class="w-2/3 pr-16">
+    <div class="mb-16 flex flex-col lg:flex-row">
+      <div class="w-full lg:w-2/3 pr-16">
         <p class="text-lg font-light text-justify">{{ artist.description }}</p>
         <h3 class="text-xl font-semibold mt-16">Biography</h3>
         {{ artist.biography }}
       </div>
-      <div class="w-1/3 flex justify-end">
+      <div
+        class="w-full lg:w-1/3 flex justify-start lg:justify-end mt-12 lg:mt-0"
+      >
         <spotify-embed v-if="artist.spotifyId" :spotify-id="artist.spotifyId" />
       </div>
     </div>
@@ -33,6 +35,7 @@
         :is-album="true"
       />
     </div>
+
     <nuxt-content :document="artist" />
   </div>
 </template>
