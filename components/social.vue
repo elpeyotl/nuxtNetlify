a3d2205e52ab74c762efa7e7b15dd3507ba0c263
Christopher Perez <christopher.perez@north-47.com>
2021-03-22 23:59:21 +0100

    added social

--- components/social.vue
+++ components/social.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <div class="social flex justify-around py-2 w-64">
-      <svg-icon name="facebook" class="icon text-white hover:text-red-500" />
-      <svg-icon name="instagram" class="icon text-white hover:text-red-500" />
-      <svg-icon name="youtube" class="icon text-white hover:text-red-500" />
-      <svg-icon name="twitter" class="icon text-white hover:text-red-500" />
-      <svg-icon name="mail" class="icon text-white hover:text-red-500" />
+      <a :href="settings.facebook" target="_blank">
+        <svg-icon name="facebook" class="icon text-white hover:text-red-500"
+      /></a>
+      <a :href="settings.instagram" target="_blank">
+        <svg-icon name="instagram" class="icon text-white hover:text-red-500" />
+      </a>
+      <a :href="settings.youtube" target="_blank">
+        <svg-icon name="youtube" class="icon text-white hover:text-red-500" />
+      </a>
+      <a :href="settings.twitter" target="_blank">
+        <svg-icon name="twitter" class="icon text-white hover:text-red-500" />
+      </a>
+      <a :href="`mailto:${settings.mail}`" target="_blank">
+        <svg-icon name="mail" class="icon text-white hover:text-red-500" />
+      </a>
     </div>
   </div>
 </template>
@@ -14,6 +24,14 @@
 import '@/icons'
 export default {
   name: 'Social',
+  data() {
+    return {
+      settings: [],
+    }
+  },
+  async mounted() {
+    this.settings = await this.$content('pages', 'settings').fetch()
+  },
 }
 </script>
 
