df3a407bf6c95b8f2ba50d2290191d6f5bd3171b
Christopher Perez <christopher.perez@north-47.com>
2021-05-05 22:39:31 +0200

    update

--- pages/team.vue
+++ pages/team.vue
@@ -5,41 +5,20 @@
         <h1 class="text-4xl font-semibold">The Yelling Light team</h1>
       </div>
     </div>
-
-    {{ team }}
-
     <div class="team">
-      <div class="w-full relative mb-4 p-2">
-        <img class="shadow-2xl team__image w-full" src="/img/me.jpg" />
-        <div class="team__caption w-2/3 max-w-md">
-          <div class="text-center mb-2 text-lg mx-auto">
-            <span class="bg-red-500 px-2 py-1"> Chris Perez </span>
-          </div>
-          <div class="text-center mb-8 text-sm mx-auto">
-            <span class="bg-red-500 px-2 py-1">Webdesigner</span>
-          </div>
-          <div class="text-center mb-2 text-sm mx-auto bg-red-500 p-2">
-            <span>
-              Music was my first love! I like geeky stuff, nature and of course
-              fucking awsome concerts!!
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div class="w-full relative mb-4 p-2">
-        <img class="shadow-2xl team__image" src="/img/puffler.jpg" />
-        <div class="team__caption w-2/3">
-          <div class="text-center mb-2 text-lg mx-auto">
-            <span class="bg-red-500 px-2 py-1"> Christan Poffet </span>
-          </div>
-          <div class="text-center mb-8 text-lg mx-auto">
-            <span class="bg-red-500 px-2 py-1">El Maestro</span>
-          </div>
-          <div class="text-center mb-2 text-sm mx-auto">
-            <span class="bg-red-500 px-2 py-1 text-center inline">
-              He's the best. Brain behind the Yelling Light. Love everywhere!
-            </span>
+      <div v-for="member in team" :key="member.name">
+        <div class="w-full relative mb-4 p-2">
+          <img class="shadow-2xl team__image w-full" :src="member.image" />
+          <div class="team__caption w-2/3 max-w-md">
+            <div class="text-center mb-2 text-lg mx-auto">
+              <span class="bg-red-500 px-2 py-1"> {{ member.name }} </span>
+            </div>
+            <div class="text-center mb-8 text-sm mx-auto">
+              <span class="bg-red-500 px-2 py-1">{{ member.job }}</span>
+            </div>
+            <div class="text-center mb-2 text-sm mx-auto bg-red-500 p-2">
+              {{ member.description }}
+            </div>
           </div>
         </div>
       </div>
