ffdb035a962272d5a2ccd67508ed8c3c6b975806
Christopher Perez <ch.perez@mac.com>
2021-05-12 09:39:01 +0200

    finished pixi animation stuff

--- components/BackgroundImage.vue
+++ components/BackgroundImage.vue
@@ -5,6 +5,8 @@
 <script>
 import { gsap, EasePack } from 'gsap/all'
 import { PixiPlugin } from 'gsap/PixiPlugin'
+import { ReflectionFilter } from 'pixi-filters'
+
 import * as PIXI from 'pixi.js'
 export default {
   name: 'BackgroundImage',
@@ -14,6 +16,7 @@ export default {
       width: null,
       height: null,
       app: null,
+      afp: null,
     }
   },
   computed: {
@@ -33,11 +36,16 @@ export default {
   },
   mounted() {
     this.init()
+    window.addEventListener('resize', this.resize)
   },
   methods: {
     init() {
-      this.initDimensions()
       this.initCanvas()
+      this.resize()
+    },
+    resize() {
+      this.initDimensions()
+      this.app.renderer.resize(this.width, this.height)
       this.clearStage()
       this.initImage()
     },
@@ -46,24 +54,30 @@ export default {
       this.height = window.innerHeight
     },
     initCanvas() {
-      this.canvas = document.getElementById('bgCanvas')
       const width = this.width
       const height = this.height
       this.app = new PIXI.Application({
         width,
         height,
-        resizeTo: window,
       })
-      // Add the view to the DOM
-      this.canvas.appendChild(this.app.view)
-      // Resizes renderer view in CSS pixels to allow for resolutions other than 1
       this.app.renderer.autoDensity = true
+      this.app.renderer.resolution = devicePixelRatio
+      this.app.renderer.autoResize = true
+      // Add the view to the DOM
+      this.canvas = document.getElementById('bgCanvas')
+      this.canvas.appendChild(this.app.view)
       const container = new PIXI.Container()
       this.app.stage.addChild(container)
       // Resize the view to match viewport dimensions
       this.app.renderer.resize(width, height)
     },
+    animate() {
+      this.afp = requestAnimationFrame(this.animate)
+      // render the container
+      this.app.renderer.render(this.app.stage)
+    },
     clearStage() {
+      cancelAnimationFrame(this.afp)
       this.app.stage.children = []
     },
     initImage() {
@@ -72,6 +86,7 @@ export default {
       loader.add('bg', this.backgroundImgSrc)
       loader.load((loader, resources) => {
         const bg = PIXI.Sprite.from(resources.bg.data)
+        bg.filters = [new ReflectionFilter({})]
         bg.anchor.x = 0.5
         bg.x = this.width / 2
         bg.anchor.y = 0.5
@@ -80,8 +95,9 @@ export default {
         bg.height = this.height
         setTimeout(() => {
           this.app.stage.addChild(bg)
+          this.animate()
         }, 500)
-        gsap.to(bg.scale, 30, {
+        gsap.to(bg.scale, 40, {
           x: 2,
           y: 2,
           repeat: -1,
